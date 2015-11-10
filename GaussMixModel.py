bdfa1d2f3309532cffce55d155fb0ea9b0502778
lukasleung <lleung@clarku.edu>
2015-11-10 17:25:49 -0500

    update

--- GaussMixModel.py
+++ GaussMixModel.py
@@ -4,18 +4,25 @@ import TwoDGauss
 class GMM:
     def __init__(self, n):
         self.n = n
-        self.models = [[int, TwoDGauss]]
+        self.models = [[float, TwoDGauss]]    # float is the weight
 
     def add_model(self, weight, model):
         self.models.append([weight, model])
 
     def change_weight(self, index, weight):
+        if (weight < 0 or weight > 1):
+            old = self.models[index]
+            old[0] = weight
+            self.models.insert(index, old)
+    
+    def change_mean(self, index, mean_x, mean_y):
         old = self.models[index]
-        old[0] = weight
+        old[1].set_mean_x(mean_x)
+        old[1].set_mean_y(mean_y)
         self.models.insert(index, old)
     
-    def change_mean(self, index, mean):
-		old = self.models[index]
-		old[0].set_mean_x(old[0], mean.x)
-		old[0].set_mean_y(old[0], mean.y)
-		self.models.insert(index, old)
+    def change_var(self, index, var_x, var_y):
+        old = self.models[index]
+        old[1].set_var_x(var_x)
+        old[1].set_var_y(var_y)
+        self.models.insert(index, old)
---
+++ TwoDDataGen.py
@@ -0,0 +1,7 @@
+
+# with normally distributed values (x,y)
+class TwoDDataGen:
+    # For each of x and y. 
+    #   initialize the mean and variance
+    def __init__(self, xs, ys):
+        
